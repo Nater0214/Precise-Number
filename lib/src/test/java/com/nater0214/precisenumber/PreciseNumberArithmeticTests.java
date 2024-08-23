16513a86ba9bd8e3b3fada4952c2e21eb9fe559e
Nater0214 <gamer@nater0214.com>
2024-08-23 11:03:50 -0400

    Modify tests

--- lib/src/test/java/com/nater0214/precisenumber/PreciseNumberArithmeticTests.java
+++ lib/src/test/java/com/nater0214/precisenumber/PreciseNumberArithmeticTests.java
@@ -9,7 +9,7 @@ public class PreciseNumberArithmeticTests {
         PreciseNumber a = new PreciseNumber(1);
         PreciseNumber b = new PreciseNumber(2);
         PreciseNumber c = a.add(b);
-        assert c.compareTo(new PreciseNumber(3)) == 0;
+        assert c.equals(new PreciseNumber(3));
     }
 
     @Test
@@ -17,6 +17,22 @@ public class PreciseNumberArithmeticTests {
         PreciseNumber a = new PreciseNumber(1);
         PreciseNumber b = new PreciseNumber(2);
         PreciseNumber c = a.subtract(b);
-        assert c.compareTo(new PreciseNumber(-1)) == 0;
+        assert c.equals(new PreciseNumber(-1));
+    }
+
+    @Test
+    void additionLarge() {
+        PreciseNumber a = new PreciseNumber(110);
+        PreciseNumber b = new PreciseNumber(89);
+        PreciseNumber c = a.add(b);
+        assert c.equals(new PreciseNumber(199));
+    }
+
+    @Test
+    void subtractionLarge() {
+        PreciseNumber a = new PreciseNumber(110);
+        PreciseNumber b = new PreciseNumber(89);
+        PreciseNumber c = a.subtract(b);
+        assert c.equals(new PreciseNumber(21));
     }
 }
